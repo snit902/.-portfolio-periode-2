940b0cec91265f51a13b64eb9ef60febe7254eba
snit902 <9028182@student.zadkine.nl>
2025-12-04 21:38:02 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,1 +1,15 @@
+// Zodra de pagina geladen is
+window.onload = function() {
+  const banner = document.getElementById("cookie-banner");
+  const button = document.getElementById("cookie-btn");
+
+  // Toon de banner
+  banner.style.display = "block";
+
+  // Klik-handler
+  button.addEventListener("click", function () {
+    console.log("Cookie-melding weggeklikt.");
+    banner.style.display = "none";
+  });
+};
 
